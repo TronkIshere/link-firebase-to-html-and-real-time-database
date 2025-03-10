581eca3d5c13407f88d48c8285d26a0e06dc1f1f
TronkIsHere <nguyenhuutrong11133@gmail.com>
2025-03-10 14:34:58 +0700

    fix some error and add console in js function

--- video 6+7/control.js
+++ video 6+7/control.js
@@ -1,81 +1,98 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyD8BUyRPGLIKP7fwSZRe04NNOijxOWxgc4",
-    authDomain: "tt-iot-c69a6.firebaseapp.com",
-    databaseURL: "https://tt-iot-c69a6-default-rtdb.firebaseio.com",
-    projectId: "tt-iot-c69a6",
-    storageBucket: "tt-iot-c69a6.firebasestorage.app",
-    messagingSenderId: "107225342345",
-    appId: "1:107225342345:web:d8cca541b9f2b012778158"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAGlAguCGqnB6wodIqHcGTYkqgY8Cfn2l8",
+  authDomain: "connect-db-f8796.firebaseapp.com",
+  projectId: "connect-db-f8796",
+  storageBucket: "connect-db-f8796.firebasestorage.app",
+  messagingSenderId: "921943184859",
+  appId: "1:921943184859:web:91294b11a69559deb103fe",
+  measurementId: "G-709P0ESPG7",
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Khởi tạo Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-  
-//Khai báo biến
+// Lấy dữ liệu nhiệt độ từ database và hiển thị
+const temp = document.getElementById("temp");
+const tempRef = ref(database, "/IoT lab/temperature");
+
+onValue(tempRef, (snapshot) => {
+  let t = snapshot.val();
+  temp.innerHTML = t;
+});
+
+// Điều khiển đèn LED 01
 const led01_on = document.getElementById("led01_on");
 const led01_off = document.getElementById("led01_off");
 const pic01 = document.getElementById("pic01");
 
-//update data temp
-const temp = document.getElementById("temp")
-database.ref("/IoT lab/temperature").on("value", function(snapshot){
-    let t = snapshot.val();
-    temp.innerHTML = t;
-})
+led01_on.onclick = function () {
+  pic01.src = "sáng-removebg-preview.png";
+  console.log("Turn on");
+  set(ref(database, "/IoT lab/Light01"), { Light01: 1 });
+};
 
-led01_on.onclick = function(){
-    pic01.src = "sáng-removebg-preview.png";
+led01_off.onclick = function () {
+  pic01.src = "tắt-removebg-preview.png";
+  console.log("Turn off");
+  set(ref(database, "/IoT lab/Light01"), { Light01: 0 });
+};
 
-     database.ref("/IoT lab/Light01").update({
-        "Light01" : 1
-     })
-}
-led01_off.onclick = function(){
-    pic01.src = "tắt-removebg-preview.png";
-
-     database.ref("/IoT lab/Light01").update({
-        "Light01" : 0
-     })
-}
-
-
+// Điều khiển đèn LED 02
 const led02_on = document.getElementById("led02_on");
 const led02_off = document.getElementById("led02_off");
 const pic02 = document.getElementById("pic02");
-led02_on.onclick = function(){
-    pic02.src = "sáng-removebg-preview.png";
-}
-led02_off.onclick = function(){
-    pic02.src = "tắt-removebg-preview.png";
-}
 
+led02_on.onclick = function () {
+  pic02.src = "sáng-removebg-preview.png";
+  console.log("Turn on");
+  set(ref(database, "/IoT lab/Light02"), { Light02: 1 });
+};
 
+led02_off.onclick = function () {
+  pic02.src = "tắt-removebg-preview.png";
+  console.log("Turn off");
+  set(ref(database, "/IoT lab/Light02"), { Light02: 0 });
+};
+
+// Điều khiển đèn LED 03
 const led03_on = document.getElementById("led03_on");
 const led03_off = document.getElementById("led03_off");
 const pic03 = document.getElementById("pic03");
-led03_on.onclick = function(){
-    pic03.src = "sáng-removebg-preview.png";
-}
-led03_off.onclick = function(){
-    pic03.src = "tắt-removebg-preview.png";
-}
 
+led03_on.onclick = function () {
+  pic03.src = "sáng-removebg-preview.png";
+  console.log("Turn on");
+  set(ref(database, "/IoT lab/Light03"), { Light03: 1 });
+};
 
+led03_off.onclick = function () {
+  pic03.src = "tắt-removebg-preview.png";
+  console.log("Turn off");
+  set(ref(database, "/IoT lab/Light03"), { Light03: 0 });
+};
+
+// Điều khiển đèn LED 04
 const led04_on = document.getElementById("led04_on");
 const led04_off = document.getElementById("led04_off");
 const pic04 = document.getElementById("pic04");
-led04_on.onclick = function(){
-    pic04.src = "sáng-removebg-preview.png";
-}
-led04_off.onclick = function(){
-    pic04.src = "tắt-removebg-preview.png";
-}
 
+led04_on.onclick = function () {
+  pic04.src = "sáng-removebg-preview.png";
+  console.log("Turn on");
+  set(ref(database, "/IoT lab/Light04"), { Light04: 1 });
+};
 
+led04_off.onclick = function () {
+  pic04.src = "tắt-removebg-preview.png";
+  console.log("Turn off");
+  set(ref(database, "/IoT lab/Light04"), { Light04: 0 });
+};
